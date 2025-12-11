41fcaeb6a38d3e0230912f4d0f4f44ec63ff778c
agafonovaevgenia808-sys <agafonovaevgenia808@gmail.com>
2025-12-11 14:12:03 +0300

    Изучение функций

--- function.js
+++ function.js
@@ -42,3 +42,111 @@ function getMax(a , b){
 getMax( 2 , 3);
 getMax( 3 , 3);
 getMax( 3 , 2);
+
+
+//Задание 4
+
+function calculate(num1, num2 , operator){
+switch (operator){
+  case '+':
+  return num1 + num2;
+  case '-':
+   return num1 - num2;
+   case '/':
+    return  num1 / num2;
+    case '*':
+      return num1 * num2;
+}
+}
+console.log( calculate (10 , 5 , '+'));
+console.log( calculate (10 , 5 , '-'));
+console.log( calculate (10 , 5 , '/'));
+console.log( calculate (10 , 5 , '*'));
+
+
+//Задаие 5 
+
+function gradeToText(grade){
+  switch(grade){
+       case 1:
+    return 'Плохо';
+       case 2:
+    return 'Плохо';
+       case 3:
+    return 'Удовлетворительно ';
+       case 4:
+    return 'Хорошо ';
+       case 5:
+    return 'Отлично';
+  }
+}
+console.log(gradeToText (1));
+console.log(gradeToText (2));
+console.log(gradeToText (3));
+console.log(gradeToText (4));
+console.log(gradeToText (5));
+
+
+//СТРЕЛОЧНЫЕ ФУНКЦИИ
+
+//Задание 1
+
+
+const isEven = (num) =>{
+return  num % 2 === 0 ? 'четное' : 'нечетное';
+}
+console.log(isEven (4));
+
+//Задание 2
+
+const temparate = (temp) =>{
+ return temp > 25 ? 'Жарко' : 'Прохладно';
+}
+console.log(temparate(26));
+
+//Задание 3
+
+const price = (res) =>{
+  return res > 1000 ? res * 0.1 : res;
+}
+console.log(price(1234));
+
+//Задание 4
+
+const seasonByMonth = (month) =>{
+switch(month){
+case 1:
+case 2:
+case 12:
+return 'Зима'
+
+case 3:
+case 4:
+case 5:
+return 'Весна'
+case 6:
+case 7:
+case 8:
+return 'Лето'
+case 9:
+case 10:
+case 11:
+  return 'Осень'
+}
+}
+console.log(seasonByMonth (6));
+
+
+//Задание 5
+
+const accessByRole = (role) => {
+  switch(role){
+    case 'admin':
+    return 'Полный доступ';
+    case 'user':
+    return 'Ограниченый доступ'
+    case '':
+    return 'Нет доступа'
+  }
+}
+console.log(accessByRole('admin'));
