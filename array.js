8c1dcf11fb76b25c2823ad1966cd7cf3ffd5ba3e
agafonovaevgenia808-sys <agafonovaevgenia808@gmail.com>
2025-12-15 11:39:44 +0300

    массивы

--- array.js
+++ array.js
@@ -45,16 +45,69 @@ console.log( result (product));
 
 //Задание 6
 
-const line = 'Ананас ,  Банан  , Киви';
-const array = line.split('Ананас , Банан , Киви');
+const line = 'Ананас, Банан, Киви';
+const array = line.split(', ');
 console.log(array);
 
-//задание 7 
+//задание 7 не понимаю как сделать
 
-const arr = ['Ананас' , 'Банан'  , 'Киви'];
-console.log(arr.join('Ананас , Банан  , Киви'));
+const arr = ['Ананас', 'Банан', 'Киви'];
+console.log(arr.join(''));
 
-
-const num = '1 , 2 , 3';
-const  num2 = num.split('1 , 2 , 3');
+//Задание 8 не понимаю как сделать
+const num = '1 2 3 4';
+const  num2 = num.split(' ');
 console.log(num2);
+
+//Задание 9
+
+const numbers = ['1' , '2' , '3'];
+const res = numbers.join('-');
+console.log(res);
+
+
+const str = 'кошка|собака|мышь';
+const strArr= str.split('|');
+console.log(strArr[0]);
+
+
+const strok = 'машина квартира дом огород';
+const words = strok
+  .split(' ')
+  .reverse()
+  .join(' ');
+
+console.log(words);
+
+
+//задание 1 forEach
+
+const wordNumber = ['1' , '2' , '3' , '4'];
+wordNumber.forEach(function(item){
+  console.log(item);
+})
+
+const wordNumbers = ['1' , '2' , '3' , '4'];
+wordNumber.forEach(function(items){
+  const results = items * 2
+  console.log(results);
+})
+
+
+const names = ['Ваня' , 'Леша' , 'Валя' , 'Женя'];
+names.forEach (names => {
+console.log(`Привет ${names}`);
+})
+
+const price = ['120' , '324' , '34' , '21'];
+let forSum = 0;
+price.forEach (prices => {
+forSum += prices;
+})
+console.log(`Общая сумма всех цен ${forSum}`);
+
+
+const km = ['1','45', '6' ,'9'];
+km.forEach (km => {
+  console.log(`Скорость ${km}`);
+})
