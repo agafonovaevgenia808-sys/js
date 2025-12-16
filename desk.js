0878e4a9db824848678aa2889f152cbd6052c3cd
agafonovaevgenia808-sys <agafonovaevgenia808@gmail.com>
2025-12-16 12:43:48 +0300

    rest

---
+++ desk.js
@@ -0,0 +1,19 @@
+const numbers = [ 1 ,2 ,3 ,4];
+const [...rest]=numbers;
+console.log(rest);
+
+
+
+
+const number = [ 1 ,2 ,3 ,4];
+const [one , ...others] = number;
+console.log(one , others);
+
+const strok = [ '1' ,'2' ,'3' ,'4'];
+const [...rests ]=strok;
+console.log(rests);
+
+
+const names = ['Ваня' , 'Леша' , 'Валя' , 'Женя'];
+const [...name1]=names;
+console.log(name1.length);
